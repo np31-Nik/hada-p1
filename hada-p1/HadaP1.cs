e5bba57440f738cd6f8b67be4fd220bab4743066
Nikita Polyanskiy <np31@alu.ua.es>
2021-02-12 18:54:25 +0100

    Modified HadaP1 (Added Minutes2Seconds)

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -14,5 +14,10 @@ namespace hada_p1
                 return 0;
             return s / 60;
         }
+
+        public static double Minutes2Seconds(double m)
+        {
+            return m * 60;
+        }
     }
 }

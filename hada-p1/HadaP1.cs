0da6edd4658dafada66f0bb4083abf8739b474c9
Nikita Polyanskiy <np31@alu.ua.es>
2021-02-24 18:31:42 +0100

    Added summary for HadaP1 class and methods

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -6,15 +6,27 @@ using System.Threading.Tasks;
 
 namespace hada_p1
 {
+    /// <summary>
+    /// Esta clase contiene metodos para convertir de segundos a minutos y viceversa
+    /// </summary>
     class HadaP1
     {
+        /// <summary>
+        /// Metodo segundos a minutos
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
         public static double Seconds2Minutes(double s)
         {
             if (s == 0)
                 return 0;
             return s / 60;
         }
-
+        /// <summary>
+        /// Metodo minutos a segundos
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
         public static double Minutes2Seconds(double m)
         {
             return m * 60;
